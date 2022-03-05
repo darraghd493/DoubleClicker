23963d809ac03ecc6b663ac0e713266d40b29705
doge2018 <60071719+dogesupremacy@users.noreply.github.com>
2022-03-05 04:40:34 +0000

    Updated build file

--- test-build.bat
+++ test-build.bat
@@ -1,24 +1,17 @@
 @echo off
-title WasdInfo Compiler
+title DoubleClicker Compiler
 :::
-:::   _____                   ___   ___  __  ___  _
-:::  |  __ \                 |__ \ / _ \/_ |/ _ \( )
-:::  | |  | | ___   __ _  ___   ) | | | || | (_) |/ ___
-:::  | |  | |/ _ \ / _` |/ _ \ / /| | | || |> _ <  / __|
-:::  | |__| | (_) | (_| |  __// /_| |_| || | (_) | \__ \
-:::  |_____/ \___/ \__, |\___|____|\___/_|_|\___/  |___/
-:::  |  __ \        __/ | |   | |     / ____| (_)    | |
-:::  | |  | | ___  |___/| |__ | | ___| |    | |_  ___| | _____ _ __
-:::  | |  | |/ _ \| | | | '_ \| |/ _ \ |    | | |/ __| |/ / _ \ '__|
-:::  | |__| | (_) | |_| | |_) | |  __/ |____| | | (__|   <  __/ |
-:::  |_____/ \___/ \__,_|_.__/|_|\___|\_____|_|_|\___|_|\_\___|_|
+:::   88PPP. 88888888 888  888 d88PPPo 888        ,d8PPPP doooooo 888         8888 doooooo 888  ,dP   ,d8PPPP   ,dbPPPp
+:::  88   8 888  888 888  888 888ooo8 888        d88ooo  d88     888         8888 d88     888o8P'    d88ooo    d88ooP'
+:::  88   8 888  888 888  888 888   8 888      ,88'      d88     888         8888 d88     888 Y8L  ,88'      ,88' P'
+:::  88oop' 888oo888 888PP888 888PPPP 888PPPPP 88bdPPP   d888888 888PPPPP    8888 d888888 888  `8p 88bdPPP   88  do
 :::
 ::: Compiler - Build into a single file
 :::
-for /f "delims=: tokens=*" %%A in ('findstr /b ::: "%~f0"') do @echo(%%A:::
+for /f "delims=: tokens=*" %%A in ('findstr /b ::: "%~f0"') do @echo(%%A
 echo Starting build
 echo.
-pyinstaller cli.py --name WasdInfo --icon icon.ico --noconfirm --noconsole --onefile
+pyinstaller cli.py --name DoubleClicker --icon icon.ico --noconfirm --noconsole --onefile
 echo.
 echo Finished build
 echo.
